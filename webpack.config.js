283d865ab3d57be0c6e0f13680f4e12dd32ef44a
T Rahul Thampi <trahulthampi@gmail.com>
2017-03-10 15:02:15 +0530

    eslint config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src') + '/app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public') + '/app',
-        filename: 'react-redux-todo.js',
-        publicPath: '/app/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                include: [
-                    path.resolve(__dirname, 'src')
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_modules')
-                ],
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015']
-                }
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
-    devServer: {
-        hot: true,
-        contentBase: path.resolve(__dirname, 'src'),
-        port: 3000,
-        compress: true,
-        overlay: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+  entry: `${path.resolve(__dirname, 'src')}/app/index.js`,
+  output: {
+    path: `${path.resolve(__dirname, 'public')}/app`,
+    filename: 'react-redux-todo.js',
+    publicPath: '/app/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'env']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
     ]
-}
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'src'),
+    port: 3000,
+    compress: true,
+    overlay: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
